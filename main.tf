1a71809db238c580e7b1590490a5ac59bbf77e73
Kube <james@globus.org>
2022-08-16 08:21:46 -0400

    Instances launching and populating Ansible inventory file

--- main.tf
+++ main.tf
@@ -18,6 +18,9 @@ resource "aws_instance" "GCSv4Nodes" {
   count = 1
   ami           = "ami-0629230e074c580f2"
   instance_type = "t2.micro"
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.GCSv4Nodes[count.index].tags.Name} ansible_host=${aws_instance.GCSv4Nodes[count.index].public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/jkube/.ssh/KUBE20211112.pem>>inventory"
+	  }
 
   tags = {
     Name = "Ubuntu-GCSv4-Node${count.index}"
@@ -27,6 +30,10 @@ resource "aws_instance" "GCSv5UbuntuNodes" {
   count = 2
   ami           = "ami-06ad98dc9c4e046bd"
   instance_type = "t2.micro"
+  provisioner "local-exec" {
+#command = "echo ${aws_instance.GCSv5UbuntuNodes[count.index].tags.Name} ansible_host=${aws_instance.GCSv5UbuntuNodes[count.index].public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/jkube/.ssh/KUBE20211112.pem>>inventory"
+    command = "echo ${self.tags.Name} ansible_host=${self.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/jkube/.ssh/KUBE20211112.pem>>inventory"
+	  }
 
   tags = {
     Name = "Ubuntu-GCSv5-Node${count.index}"
@@ -36,8 +43,16 @@ resource "aws_instance" "GCSv5CentosNodes" {
   count = 1
   ami           = "ami-000e7ce4dd68e7a11"
   instance_type = "t2.micro"
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.GCSv5CentosNodes[count.index].tags.Name} ansible_host=${aws_instance.GCSv5CentosNodes[count.index].public_ip} ansible_user=centos ansible_ssh_private_key_file=/home/jkube/.ssh/KUBE20211112.pem>>inventory"
+	  }
+#;provisioner "local-exec" {
+#;command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u centos -i aws_instance.  --private-key /home/jkube/.ssh/id_rsa -e /home/jkube/workspace/git/Globus/ansible/TST/globus_GCS_centosSetup.yml"
+#;}
+
 
   tags = {
     Name = "Centos-GCSv5-Node${count.index}"
   }
 }
+
