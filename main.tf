884ba46863578738bb2e22428a67cb51ce0cd070
Kube <james@globus.org>
2022-08-20 05:32:42 -0400

    Added in TF check for instance reachability before calling local-exec of ansible playbook, but need to update playbook to use "expect," ansible calls failing due to "interactive" mode not being set

--- main.tf
+++ main.tf
@@ -17,7 +17,9 @@ provider "aws" {
 resource "aws_instance" "GCSv4Nodes" {
   count = 1
   ami           = "ami-0629230e074c580f2"
+  key_name = "KUBE20211112"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-061065abfa8528d29"]
   provisioner "local-exec" {
     command = "echo ${aws_instance.GCSv4Nodes[count.index].tags.Name} ansible_host=${aws_instance.GCSv4Nodes[count.index].public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/jkube/.ssh/KUBE20211112.pem>>inventory"
 	  }
@@ -29,7 +31,9 @@ resource "aws_instance" "GCSv4Nodes" {
 resource "aws_instance" "GCSv5UbuntuNodes" {
   count = 2
   ami           = "ami-06ad98dc9c4e046bd"
+  key_name = "KUBE20211112"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-061065abfa8528d29"]
   provisioner "local-exec" {
 #command = "echo ${aws_instance.GCSv5UbuntuNodes[count.index].tags.Name} ansible_host=${aws_instance.GCSv5UbuntuNodes[count.index].public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/jkube/.ssh/KUBE20211112.pem>>inventory"
     command = "echo ${self.tags.Name} ansible_host=${self.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/jkube/.ssh/KUBE20211112.pem>>inventory"
@@ -42,7 +46,9 @@ resource "aws_instance" "GCSv5UbuntuNodes" {
 resource "aws_instance" "GCSv5CentosNodes" {
   count = 1
   ami           = "ami-000e7ce4dd68e7a11"
+  key_name = "KUBE20211112"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-061065abfa8528d29"]
   provisioner "local-exec" {
     command = "echo ${aws_instance.GCSv5CentosNodes[count.index].tags.Name} ansible_host=${aws_instance.GCSv5CentosNodes[count.index].public_ip} ansible_user=centos ansible_ssh_private_key_file=/home/jkube/.ssh/KUBE20211112.pem>>inventory"
 	  }
@@ -53,6 +59,21 @@ resource "aws_instance" "GCSv5CentosNodes" {
 
   tags = {
     Name = "Centos-GCSv5-Node${count.index}"
+  }
+}
+
+resource "null_resource" "nullremote2" {
+depends_on = [aws_instance.GCSv4Nodes,aws_instance.GCSv5UbuntuNodes,aws_instance.GCSv5CentosNodes]  
+  provisioner "remote-exec" {
+    connection {
+      host = aws_instance.GCSv4Nodes[0].public_dns
+      user = "ubuntu"
+      private_key = file("/home/jkube/.ssh/KUBE20211112.pem")
+    }
+    inline = ["echo 'GCSv4Ubuntu nodes online!'"]
+  }
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory /home/jkube/workspace/git/Globus/ansible/TST/globus_GCS_allDistros.yml"
   }
 }
 
